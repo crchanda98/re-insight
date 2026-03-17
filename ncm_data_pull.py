993ba45e7dd48b65c7fce0dc3668d0ce1823e31e
arijit <crchanda98@gmail.com>
2026-03-18 01:01:55 +0530

    Add API-based data handle

--- ncm_data_pull.py
+++ ncm_data_pull.py
@@ -11,17 +11,22 @@ with open(CONFIG_PATH, "r") as f:
     config = yaml.safe_load(f)
 username = config["ncm_user"]
 password = config["ncm_password"]
+base_url=config["base_url"]
+data_func = utils.APICon(base_url = base_url)
 
 url = "https://pdscloud.ncmrwf.gov.in:8443/api/v1/REdownload"
 temp_dir = os.path.join(config["temp_dir"], "ncm_data")
 
-df_static = pd.read_csv(
-    config["static_data_path"],
-    usecols=["id", "plant_type", "latitude", "longitude", "altitude"],
-)
+df_static = pd.DataFrame.from_records(data_func.fetch_static_data())
+
 csv_path = config["ncm_csv_data"]
+ncm_temp_data= config["ncm_temp_data"]
+
 if not os.path.exists(temp_dir):
     os.makedirs(temp_dir)
+
+if not os.path.exists(ncm_temp_data):
+    os.makedirs(ncm_temp_data)
 
 manifest = []
 if os.path.exists(config["ncm_data_log"]):
@@ -30,17 +35,18 @@ if os.path.exists(config["ncm_data_log"]):
     if len(manifest) > 0:
         manifest = manifest.split("\n")
 
-lag_days = 1
+lag_days = 10
 time_now = dt.now()
 date_end = time_now.date()
 date_start = date_end - timedelta(days=lag_days)
 dates_str = [
     date_start + timedelta(days=i) for i in range((date_end - date_start).days + 1)
 ]
+
 dates_str = [x.strftime("%Y%m%d") for x in dates_str]
-dates_str.reverse()  # Process latest date first
+# dates_str.reverse()  # Process latest date first
 cycle = ["00", "06", "12", "18"]
-cycle.reverse()  # Process latest cycle first
+# cycle.reverse()  # Process latest cycle first
 
 for idate in dates_str:
     print(f"Processing date: {idate}")
@@ -57,12 +63,12 @@ for idate in dates_str:
                 continue
             else:
                 file_path = os.path.join(temp_dir, filename)
-                destination_folder = "/Users/arijitchanda/Desktop/work/git_arijit/data_lake/re_insights/temp/ncm_untar"  # Optional: specify where to extract
                 df_ncm = utils.extract_ncm(
-                    fname=file_path, dest=destination_folder, df_stn=df_static
+                    fname=file_path, dest=ncm_temp_data, df_stn=df_static
                 )
                 csv_file_path = os.path.join(csv_path, f"ncm_{date_name}.csv")
                 df_ncm.to_csv(csv_file_path, index=False)
+                data_func.upload_weather_data(df_ncm)
                 manifest.append(date_name)
                 with open(config["ncm_data_log"], "w") as f:
                     f.write("\n".join(manifest))
