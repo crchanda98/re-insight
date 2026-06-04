b3dbed5e6a24c3c10c013ac8b7758997462a8b58
devnull404 <crchanda98@gmail.com>
2026-06-04 21:57:34 +0530

    Add meas aggregator

--- aggrigator.py
+++ aggrigator.py
@@ -1,5 +1,3 @@
-from sqlalchemy import exc
-from fct_dispatch import idf
 import pandas as pd
 import yaml
 import os
@@ -7,8 +5,6 @@ import traceback
 import numpy as np
 import utils
 from datetime import datetime as dt, timedelta
-from sklearn.ensemble import RandomForestRegressor
-from ftplib import FTP
 from sqlalchemy import create_engine
 from urllib.parse import quote as urlquote
 
@@ -19,9 +15,11 @@ with open(CONFIG_PATH, "r") as f:
 SCRIPT_NAME = os.path.basename(__file__)
 
 date_now_ist = utils.get_last_15_min_slot()
-date_now = date_now_ist - timedelta(hours = 5, minutes = 30)
-fct_start_time = date_now - timedelta(hours = 1, minutes = 30)
-fct_end_time = fct_start_time.replace(hour = 23, minute = 45)
+date_now_utc = date_now_ist - timedelta(hours = 5, minutes = 30)
+
+
+fct_end_time = date_now_utc + - timedelta(hours = 6)
+fct_start_time = fct_end_time - timedelta(hours = 1)
 
 FTP_HOST = config["fct_ftp_cred"]["host"]
 FTP_USER = config["fct_ftp_cred"]["user"]
@@ -34,8 +32,13 @@ engine = create_engine(
     % urlquote(db_cred["user_passwd"])
 )
 db_columns = config["db_columns"]
+
 fct_table_column = db_columns["forecast_table"]["columns"]
 fct_table_column_un = db_columns["forecast_table"]["unique_constraint"]
+
+meas_table_column = db_columns["meas_table"]["columns"]
+meas_table_column_un = db_columns["meas_table"]["unique_constraint"]
+
 db_con = utils.DBcon(con = engine, db_schema=db_columns)
 
 db_con.logging({"script": SCRIPT_NAME, "log_type": "info", "message": f"Intraday aggregator script started"})
@@ -43,29 +46,34 @@ db_con.logging({"script": SCRIPT_NAME, "log_type": "info", "message": f"Intraday
 df_static = db_con.get_static_data()
 df_hybrid = df_static[df_static["parent_id"] == 0]
 
-group_cols = [
+group_cols_model = [
     "prediction_time",
     "forecast_time",
     "forecast_source",
     "model_name",
 ]
 
+group_cols_meas = [
+    "record_time",
+]
+
 for _, idf_parent in df_hybrid.iterrows():
     try:
         df_static_child = df_static[df_static["parent_id"] == idf_parent['plant_id']]
         db_con.logging({"script": SCRIPT_NAME, "log_type": "info", "message": f"Running aggrigator for {idf_parent['plant_name']}"})
+        
         df_hybrid = []
-        print(f"Running aggrigator for {idf_parent['plant_name']}")
+        print(f"Running model aggrigator for {idf_parent['plant_name']}")
         for _, idf_child in df_static_child.iterrows():
             farm_name = idf_child["plant_name"]
-            farm_name = idf["plant_name"]
-            fct_data = db_con.get_fct_data(plant=farm_name, fct_src="inhouse", model_name="intraday_wind", \
-                start_date=fct_start_time.strftime("%Y-%m-%dT%H:%M:%S"), end_date=fct_end_time.strftime("%Y-%m-%dT%H:%M:%S"))
-            fct_data = fct_data.drop(['created_at', 'plant_name', 'plant_id'], axis = 1)
-            df_hybrid.append(fct_data)
+            for imodel in ["om_xgb", "intraday_wind"]:
+                fct_data = db_con.get_fct_data(plant=farm_name, fct_src="inhouse", model_name=imodel, \
+                    start_date=fct_start_time.strftime("%Y-%m-%dT%H:%M:%S"), end_date=fct_end_time.strftime("%Y-%m-%dT%H:%M:%S"))
+                fct_data = fct_data.drop(['created_at', 'plant_name', 'plant_id'], axis = 1)
+                df_hybrid.append(fct_data)
         df_hybrid = pd.concat(df_hybrid)
         df_hybrid = (
-            df_hybrid.groupby(group_cols)
+            df_hybrid.groupby(group_cols_model)
             .agg({"active_power": "sum", "wind_speed": "mean",  'ghi': "mean", 'precipitation': "mean"})
             .reset_index()
         )
@@ -74,7 +82,37 @@ for _, idf_parent in df_hybrid.iterrows():
         df_hybrid = df_hybrid[fct_table_column]
         df_hybrid = df_hybrid.set_index(fct_table_column_un)
         db_con.push_fct_data(df_hybrid)
-        db_con.logging({"script": SCRIPT_NAME, "log_type": "success", "message": f"Pushed aggregated data for {farm_name}"})
+        db_con.logging({"script": SCRIPT_NAME, "log_type": "success", "message": f"Pushed aggregated forecast data for {farm_name}"})
+
+
+        #### AGGREGATE MEAS
+        df_hybrid = []
+        print(f"Running meas aggrigator for {idf_parent['plant_name']}")
+        for _, idf_child in df_static_child.iterrows():
+            farm_name = idf_child["plant_name"]
+            df_meas = db_con.get_meas_data(plant=farm_name, start_date=fct_start_time.strftime("%Y-%m-%dT%H:%M:%S"), \
+                end_date=fct_end_time.strftime("%Y-%m-%dT%H:%M:%S"))
+
+            df_meas = df_meas.drop(['created_at', 'plant_name', 'plant_id'], axis = 1)
+            df_hybrid.append(df_meas)
+        df_hybrid = pd.concat(df_hybrid)
+        df_hybrid = (
+            df_hybrid.groupby(group_cols_meas)
+            .agg({"active_power": "sum", "wind_speed": "mean",  
+                "wind_direction": "mean",
+                "ghi": "mean",
+                "gii": "mean",
+                "humidity": "mean",
+                "temperature": "mean",
+                'precipitation': "mean"})
+            .reset_index()
+        )
+        df_hybrid["plant_name"] = idf_parent["plant_name"]
+        df_hybrid["plant_id"] = idf_parent["plant_id"]
+        df_hybrid = df_hybrid[meas_table_column]
+        df_hybrid = df_hybrid.set_index(meas_table_column_un)
+        db_con.push_meas_data(df_hybrid)
+        db_con.logging({"script": SCRIPT_NAME, "log_type": "success", "message": f"Pushed aggregated meas data for {farm_name}"})
     except Exception as e:
         e = traceback.format_exc()
         db_con.logging({"script": SCRIPT_NAME, "log_type": "error", "message": f"Aggregator failed for {farm_name}: {e}"})
